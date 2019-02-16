eb9ead1e4173e1d083332c482ade363b20d40564
Jonathan Bouwer <Jonathan.Bouwer1@gmail.com>
2019-02-16 17:59:42 +0200

    Fixed flac_to_mp3.bat and added conditional output folder creation

--- flac_to_mp3.bat
+++ flac_to_mp3.bat
@@ -5,5 +5,5 @@ rem in a directory and run ffmpeg to change convert
 rem them to 320kbps mp3
 rem Output is saved in a new directory called output
 
-mkdir output
-for %%f in (*.mp3) do ffmpeg -i "%%f" -ab 320k -map_metadata 0 -id3v2_version 3 "output/%%f"
+if not exist output mkdir output
+for %%f in (*.flac) do ffmpeg -i "%%f" -ab 320k -map_metadata 0 -id3v2_version 3 "output/%%~nf.mp3"
--- swap_languages_order.bat
+++ swap_languages_order.bat
@@ -4,5 +4,5 @@ rem This script will swap track 0 and track 1 of audio and subtitles
 rem and then default track 0 of both audio and subtitles in the output
 rem Output is saved in a new directory called output
 
-mkdir output
+if not exist output mkdir output
 for %%f in (*.mkv) do ffmpeg -i "%%f" -map 0:v:0 -map 0:a:1 -map 0:a:0 -map 0:s:1 -map 0:s:0 -c copy -disposition:a:0 default -disposition:a:1 none -disposition:s:0 default -disposition:s:1 none "output/%%f"
