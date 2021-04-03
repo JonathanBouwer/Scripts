f00d0c721b3723d89fab6ef632309f257866a93c
Jonathan Bouwer <Jonathan.Bouwer1@gmail.com>
2021-04-03 14:41:56 +0200

    Fixes bug in sorting script

--- sort_videos.bat
+++ sort_videos.bat
@@ -18,12 +18,13 @@ def sort_files():
     videos = folders.pop('')
     for folder_name, folder_contents in folders.items():
         for file_name in videos:
-            if folder_name in file_name:
-                print(f'Moving "{file_name}" to "{folder_name}"')
-                if file_name in folder_contents:
-                    print(f'"{folder_name}/{file_name}" already exists! Skipping')
-                    return
-                os.rename(file_name, f'{folder_name}/{file_name}')
+            if folder_name not in file_name:
+                continue
+            if file_name in folder_contents:
+                print(f'"{folder_name}/{file_name}" already exists! Skipping')
+                continue
+            print(f'Moving "{file_name}" to "{folder_name}"')
+            os.rename(file_name, f'{folder_name}/{file_name}')
 
 if __name__=='__main__':
     sort_files()
