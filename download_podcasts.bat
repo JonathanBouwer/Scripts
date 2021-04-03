b0cfdb417142da941f4ba2bd688e048330c07b12
Jonathan Bouwer <Jonathan.Bouwer1@gmail.com>
2021-04-03 09:47:17 +0200

    Updates download_podcasts to support parallelism

--- download_podcasts.bat
+++ download_podcasts.bat
@@ -6,6 +6,7 @@ exit /b %errorlevel%
 """
 
 import xml.etree.ElementTree as ET
+from concurrent.futures import ThreadPoolExecutor
 from urllib.request import urlopen, Request
 from os import listdir, mkdir
 from os.path import exists as dir_exists, isfile
@@ -17,23 +18,35 @@ podcasts = [
   # {
   #     'name'   : NAME, 
   #     'url'    : RSS_FEED,
-  #     'rename' : lambda x, i: '{} - {}'.format(i-1, x),
-  #     'look_back' : 0 (Can be excluded)
+  #     'rename' : lambda x, i: f'{x}',
+  #     'look_back' : 0 # (Can be excluded)
   # },
 ]
 
 def get_episode_list(podcast_url):
-    request = Request(podcast_url, headers={'User-Agent' : "Magic Browser"})
+    request = Request(podcast_url, headers={'User-Agent' : 'Magic Browser'})
     xml_data = urlopen(request).read()
-    root = ET.fromstring(xml_data)
     episodes = []
-    for item in root.iter('item'):
+    for item in ET.fromstring(xml_data).iter('item'):
         enclosure = item.find('enclosure')
-        episodes.append({'title': item.find('title').text, 'url': enclosure.get('url'), 'size': int(enclosure.get('length', default='0'))})
-        
+        length = 0
+        try:
+            length = int(enclosure.get('length', default='0'))
+        except:
+            pass
+
+        try:
+            episodes.append({
+                'title': item.find('title').text,
+                'url': enclosure.get('url'),
+                'size': length
+            })
+        except:
+            print('Couldn\'t find url for ' + item.find('title').text)
+
     episodes.reverse()
     return episodes
-    
+
 def print_download_info(size, gotten, speed, LOADING_BAR_SIZE = 20):
     percent = floor(gotten / size * LOADING_BAR_SIZE)
     time_remaining = (size - gotten) // speed
@@ -58,7 +71,18 @@ def print_summary(size, duration):
     minutes_taken = int(duration // 60)
     duration = int(duration % 60)
     print('\r', end='')
-    print('Download complete | {} Kb | {} Kb/s | {}m{}s{}'.format(size, speed, minutes_taken, duration, ' '*30))
+    print('Download complete | {} Kb | {} Kb/s | {}m{}s{}\n'.format(size, speed, minutes_taken, duration, ' '*30))
+
+def fetch_podcast_feed(podcast):
+    print('Checking ' + podcast['name'])
+
+    if not dir_exists(podcast['name']):
+        mkdir(podcast['name'])
+
+    gotten_episodes = [item for item in listdir(podcast['name']) if isfile(podcast['name'] + '/' + item)]
+    gotten_episodes = gotten_episodes + [item for item in listdir() if isfile(item)]
+
+    return podcast, gotten_episodes, get_episode_list(podcast['url'])
 
 def download_episode(episode, filename, CHUNK_SIZE = 128 * 1024, UPDATE_FREQUENCY = 10):
     print('Downloading: {}'.format(episode['title']))    
@@ -66,63 +90,54 @@ def download_episode(episode, filename, CHUNK_SIZE = 128 * 1024, UPDATE_FREQUENC
     response = urlopen(request)
     episode['size'] = int(response.headers['content-length'])
     got = 0
-    
+
     start_time = perf_counter()
-    file = open(filename, 'wb')
-    while True:
-        chunk = response.read(CHUNK_SIZE)
-        downloaded = UPDATE_FREQUENCY * CHUNK_SIZE
-        if got % downloaded == 0:
-            speed = got / (perf_counter() - start_time)
-        if not chunk:
-            break
-        file.write(chunk)
-        got += CHUNK_SIZE
-        print_download_info(episode['size'], got, max(speed, 1))
-    file.close()
+    with open(filename, 'wb') as file:
+        while True:
+            chunk = response.read(CHUNK_SIZE)
+            if got % (UPDATE_FREQUENCY * CHUNK_SIZE) == 0:
+                speed = got / (perf_counter() - start_time)
+            if not chunk:
+                break
+            file.write(chunk)
+            got += CHUNK_SIZE
+            print_download_info(episode['size'], got, max(speed, 1))
     print_summary(episode['size'], perf_counter() - start_time)
-    
-illegal_chars = ['\\', '/', ':', '*', '?', '"', '<', '>', '|']
+
+illegal_chars = ['\\', '/', ':', '*', '?', '"', '<', '>', '|', '\n']
 def main():
     print('Checking for new podcasts')
     to_download = []
-    for podcast in podcasts:
-        print('Checking ' + podcast['name'])
-        if not dir_exists(podcast['name']):
-            mkdir(podcast['name'])
-        episodes = get_episode_list(podcast['url'])
-        gotten_episodes = [item for item in listdir(podcast['name']) if isfile(podcast['name'] + '/' + item)]
-        gotten_episodes = gotten_episodes + [item for item in listdir() if isfile(item)]
-    
-        if podcast.get('look_back', 0) > 0:
-            episodes = episodes[-podcast.get('look_back'):]
-    
-        new_episodes = False
-        for index, episode in enumerate(episodes, 1):
-            filename = episode['title']
-            try:
-                filename = podcast['rename'](filename, index)
-            except:
-                continue
-            for char in illegal_chars:
-                filename = filename.replace(char, '')
-            filename = filename + '.mp3'
+    with ThreadPoolExecutor() as executor:
+        for podcast, gotten_episodes, episodes in executor.map(fetch_podcast_feed, podcasts):
+            if podcast.get('look_back', 0) > 0:
+                episodes = episodes[-podcast.get('look_back'):]
             
-            if filename in gotten_episodes:
-                continue
-            new_episodes = True
-            to_download.append((episode, filename))
-            print('New Episode: {} ({})'.format(episode['title'], filename))
-        if new_episodes:
-            print()
-    
+            new_episodes = False
+            for index, episode in enumerate(episodes, 1):
+                filename = episode['title']
+                try:
+                    filename = podcast['rename'](filename, index)
+                except:
+                    continue
+                for char in illegal_chars:
+                    filename = filename.replace(char, '')
+                filename = filename + '.mp3'
+
+                if filename in gotten_episodes:
+                    continue
+                new_episodes = True
+                to_download.append((episode, filename))
+                print('New Episode: {} ({})'.format(episode['title'], filename))
+            if new_episodes:
+                print()
+
     if len(to_download) == 0:
         print('No new podcast episodes to download')
         return
-    
+
     for episode, filename in to_download:
         download_episode(episode, filename)
-        print()
-        
-    
-main()
+
+if __name__ == "__main__":
+    main()
