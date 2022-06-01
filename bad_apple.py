65c12e08b163e8fc34b5bb5ecb5108f850cb844f
Jonathan Bouwer <Jonathan.Bouwer1@gmail.com>
2022-06-01 20:03:51 +0200

    Update Bad Apple for higher quality and smoother playback

--- bad_apple.py
+++ bad_apple.py
@@ -1,44 +1,57 @@
-from urllib.request import urlopen, Request
-from os.path import exists
-import json
-import time
-import os
+from math import floor
+from shutil import get_terminal_size
+from struct import unpack
+from time import perf_counter, sleep
 
-FILE_NAME = "bad_apple_data.json"
-CHRS = " ░▒▓"
-FRAME_RATE = 18 # Video is native 18 FPS
-REFRESH_FREQ = 1 / FRAME_RATE
+def read_data():
+    FILE_NAME = 'raw_bad_apple.bin'
+    NUM_FRAMES = 6569
+    WIDTH = 72
+    HEIGHT = 54
 
-data = {}
-if exists(FILE_NAME):
-    with open(FILE_NAME) as f:
-      data = json.load(f)
-else:
-    req = Request("https://adryd.co/tba3")
-    raw_data = urlopen(req).read().decode('utf-8')
-    data = json.loads(raw_data)
+    with open(FILE_NAME, 'rb') as f:
+        return [
+            # Frames are defined bottom up, but displayed top down so reverse rows
+            [unpack(f'{WIDTH}B', f.read(WIDTH)) for _ in range(HEIGHT)][::-1]
+            for i in range(NUM_FRAMES)
+        ]
 
-    with open(FILE_NAME, "w") as f:
-      f.write(raw_data)
+def render_video(frames, frame_rate):
+    MAX_LUMINANCE = 256
+    CHRS = ' ░▒▓█' # Shading characters
+    PROGRESS_BAR_SIZE = 2 * len(frames[0][0]) - 2 # Pixel = 2 chrs, Ends = '[', ']'
 
-term_size = os.get_terminal_size()
-lines = term_size.lines
+    i = 0
+    start_time = perf_counter()
+    while i < len(frames):
+        # Simulate
+        frame_buffer = [
+            ''.join([f'{CHRS[floor(val / MAX_LUMINANCE * len(CHRS))]}' * 2 for val in row])
+            for row in frames[i]
+        ]
 
-start_time = time.perf_counter()
-expected_time = 0
-for i, block in enumerate(data['c']):
-  frame_start = time.perf_counter()
-  expected_time = start_time + i * REFRESH_FREQ
-  if (time.perf_counter() - expected_time > REFRESH_FREQ):
-    continue
+        percent = PROGRESS_BAR_SIZE * i // len(frames)
+        frame_buffer.append(f'[{"#" * percent}{" " * (PROGRESS_BAR_SIZE - percent)}]')
 
-  out = ""
-  for col_idx in range(len(block)):
-    row = [row[col_idx] for row in block]
-    out += "".join([f'{CHRS[val // 64]}'*2 for val in row]) + "\n"
-  os.system('cls')
-  print(out, flush=True)
+        frame_buffer.append('\n' * (get_terminal_size().lines - len(frame_buffer)))
 
-  frame_time = time.perf_counter() - frame_start
-  if frame_time < REFRESH_FREQ:
-    time.sleep(REFRESH_FREQ - frame_time)
+        # Render
+        print('\n'.join(frame_buffer), end='', flush=True)
+
+        # Sleep (or skip frames)
+        current_time = perf_counter()
+        expected_time = start_time + i / frame_rate
+        if current_time < expected_time:
+            sleep(expected_time - current_time)
+        else:
+            i += floor((current_time - expected_time) * frame_rate)
+
+        i += 1
+    print('\n' * get_terminal_size().lines)
+
+def main():
+    FRAME_RATE = 30
+    frames = read_data()
+    render_video(frames, FRAME_RATE)
+    
+main()
